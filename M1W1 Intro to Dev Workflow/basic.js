53f5968b2c35586f18fe365b427d74abdb6ffd66
Alvin Ng <alvin.cl.ng@gmail.com>
2023-10-21 11:28:43 -0700

    Made working function

---
+++ M1W1 Intro to Dev Workflow/basic.js
@@ -0,0 +1,25 @@
+const username = "Alvin"
+
+// let username2 = "Alvin"
+
+// console.log(username)
+// console.log(username2)
+
+// console.log(username)
+
+// const namesArr = ["Joe", "Katie", "Sarah"];
+
+// namesArr.push("Ryan")
+
+// console.log(namesArr)
+
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+console.log(addTwoNumbers(10, 10) + 100)
+
+const four = 4;
+
+console.log(four + 6)
+
+console.log(username + " How are you?")
